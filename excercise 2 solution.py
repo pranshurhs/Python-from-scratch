93ce2de150a81883e6aadca07894a391b7b14e54
pranshurhs <2077pranshu@gmail.com>
2026-06-09 23:24:12 +0530

    Add files via upload

---
+++ excercise 2 solution.py
@@ -0,0 +1,10 @@
+#excercise 2
+n = int(input("enter a number:  e").strip())
+if n % 2!= 0:
+    print('Weird')
+if n % 2 == 0 and 2 <= n <= 5:
+    print('Not Weird')
+if n % 2 == 0 and 6 <= n <= 20:
+    print('Weird')
+if n % 2 == 0 and n > 20:
+    print('Not Weird')
