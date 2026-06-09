6c510be8a3e46dd27edfa75bd605b6a68876577a
pranshurhs <2077pranshu@gmail.com>
2026-06-09 23:35:00 +0530

    Add files via upload

---
+++ excercise 3 solution.py
@@ -0,0 +1,11 @@
+#excercise 3 sort of easy 
+a = int(input('enter a non infinite number:  '))
+b = int(input('enter a non infinite number:  '))
+1 <= a <= 10**10
+1 <= b <= 10**10
+X = a + b
+Y = a - b
+Z = a * b
+print(X)
+print(Y)
+print(Z)
